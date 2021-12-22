1b1f1c8f30a771dfd4f4ce6c683ac9f32eb649c9
AndoniGuzman <andoni042295@gmail.com>
2021-12-22 14:04:47 -0600

    Update uploadToDatabase.py

--- uploadToDatabase.py
+++ uploadToDatabase.py
@@ -2,14 +2,18 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+
 
 def print_hello():
-    return 'Hello world from first Airflow DAG 2!'
+    return 'Hello world from first Airflow DAG!'
 
-dag = DAG('hello_world2', description='Hello World DAG',
+dag = DAG('Data upload to postgreSQL', description='Read a csv and upload it to a postgresSQL',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
-hello_operator = PythonOperator(task_id='hello_task2', python_callable=print_hello, dag=dag)
+#hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+installPipDependencies = BashOperator(task_id='installPipDependencies', bash_command="pip install psycopy2-binary ; pip install google-cloud-storage",
+                                      xcom_push=True, dag = dag)
 
-hello_operator
+installPipDependencies
