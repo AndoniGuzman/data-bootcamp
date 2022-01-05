d3b46c742ad24cc7e1aaa029bd37397ee17f128c
AndoniGuzman <andoni042295@gmail.com>
2022-01-05 13:43:57 -0600

    Update movieReviewDag.py

--- movieReviewDag.py
+++ movieReviewDag.py
@@ -17,7 +17,7 @@ def readFileFromBucket(bucketName, fileName):
     blob.download_to_filename("test.csv")
 
 #Dags
-dag = DAG('Movie review Logic DAG', description='Moview review logic DAG',
+dag = DAG('MovieReviewLogicDAG', description='Moview review logic DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
@@ -26,7 +26,7 @@ dag = DAG('Movie review Logic DAG', description='Moview review logic DAG',
 
 command = "python3 movieReviewLogic.py --input gs://de-bootcamp-ag-raw/movie_review.csv --output gs://de-bootcamp-ag-stagin/movie_review/output --runner DataflowRunner --project de-bootcamp-ag --region us-central1  --temp_location gs://de-bootcamp-ag-stagin/tmp/"
 
-runDataflowJob = BashOperator(task_id='runMovieReviewLogic', bash_command=command,dag = dag)
+runDataflowJob = BashOperator(task_id='runMovieReviewLogic', bash_command="ls -ltr /opt/airflow/dags/repo/",dag = dag)
 
 readFile = download_file = GCSToLocalFilesystemOperator(
         task_id="getFileFromBucket",
@@ -35,4 +35,4 @@ readFile = download_file = GCSToLocalFilesystemOperator(
         filename="movieReviewLogic.py",
     )
 
-readFile >> runDataflowJob
+runDataflowJob
