a88a035c70818f95b32a70587812f6418cbd3780
AndoniGuzman <andoni042295@gmail.com>
2022-01-05 12:42:44 -0600

    Add dag for movie review

---
+++ movieReviewDag.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.providers.google.cloud.transfers.gcs_to_local import *
+from airflow.hooks.postgres_hook import PostgresHook
+
+#Dags
+dag = DAG('Movie review Logic DAG', description='Moview review logic DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+
+# Tasks
+
+command = """python3 movieReviewLogic.py 
+              --input gs://de-bootcamp-ag-raw/movie_review.csv
+              --output gs://de-bootcamp-ag-stagin/movie_review/output
+              --runner DataflowRunner
+              --project de-bootcamp-ag
+              --region us-central1 
+              --temp_location gs://de-bootcamp-ag-stagin/tmp/
+          """
+
+runDataflowJob = BashOperator(task_id='runMovieReviewLogic', bash_command=command,dag = dag)
+
+runDataflowJob
