11d941e16189326d003b7640242043c01ca0a713
AndoniGuzman <andoni042295@gmail.com>
2022-01-05 14:48:30 -0600

    Update movieReviewDag.py

--- movieReviewDag.py
+++ movieReviewDag.py
@@ -5,18 +5,37 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.providers.google.cloud.transfers.gcs_to_local import *
 
+
+from google.cloud import storage
+from google.cloud.storage import blob
+
+def readFileFromBucket(bucketName, fileName):
+    storageClient = storage.Client()
+    bucket = storageClient.bucket(bucketName)
+    blob = bucket.blob(fileName)
+
+    blob.download_to_filename("test.csv")
+
 #Dags
 dag = DAG('MovieReviewLogicDAG', description='Moview review logic DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
+
 # Tasks
-command = "python /opt/airflow/dags/repo/movieReviewLogic.py --input gs://de-bootcamp-ag-raw/movie_review.csv --output gs://de-bootcamp-ag-stagin/movie_review/output --runner DataflowRunner --project de-bootcamp-ag --region us-central1  --temp_location gs://de-bootcamp-ag-stagin/tmp/"
 
-runDataflowJob = BashOperator(task_id='runMovieReviewLogic', bash_command=command,dag = dag)
+movieReview = BeamRunPythonPipelineOperator(
+    task_id="moviewReview",
+    py_file="/opt/airflow/dags/repo/movieReviewLogic.py",
+    py_options=[],
+    pipeline_options={
+        'output': "gs://de-bootcamp-ag-stagin/results/movieReview/output",
+    },
+    py_requirements=['apache-beam[gcp]>=2.21.0'],
+    py_interpreter='python3',
+    py_system_site_packages=False,
+    dataflow_config={'location': 'us-central1',
+                     'project_id': 'de-bootcamp-ag'}
+)
 
-command = "gcloud auth application-default login"
-
-getAppPermissions = BashOperator(task_id='getAppPermissions', bash_command=command,dag = dag)
-
-getAppPermissions >> runDataflowJob
+movieReview 
