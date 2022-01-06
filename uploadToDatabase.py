97e6dc68707044805c5601eccef2e52808ae9c78
AndoniGuzman <andoni042295@gmail.com>
2022-01-06 12:30:05 -0600

    Update uploadToDatabase.py

--- uploadToDatabase.py
+++ uploadToDatabase.py
@@ -71,7 +71,7 @@ dag = DAG('DataUploadToPostgreSQL', description='Read a csv and upload it to a p
 
 # Tasks
 
-command = "pip install bs4 ;pip install psycopg2-binary ; pip install google-cloud-storage; pip install wheel; pip install 'apache-beam[gcp]'; pip install apache-airflow-providers-apache-beam; pip install pip install apache-airflow-providers;pip install apache-airflow-providers-apache-beam[google]"
+command = "pip install bs4 ;pip install psycopg2-binary ; pip install google-cloud-storage; pip install wheel; pip install 'apache-beam[gcp]'; pip install apache-airflow-providers-apache-beam; pip install pip install apache-airflow-providers;pip install apache-airflow-providers-apache-beam[google] --use-deprecated=legacy-resolver"
 installPipDependencies = BashOperator(task_id='installPipDependencies', bash_command=command,dag = dag)
 
 #readFile = PythonOperator(task_id='getFileFromBucket', python_callable=readFileFromBucket("de-bootcamp-ag","user_purchase.csv"), dag=dag) 
