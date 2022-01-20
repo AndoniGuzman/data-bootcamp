7b70499239224b26e111a8db2f70a67b5439b544
AndoniGuzman <andoni042295@gmail.com>
2022-01-20 14:39:07 -0600

    Update uploadToDatabase.py

--- uploadToDatabase.py
+++ uploadToDatabase.py
@@ -6,36 +6,22 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.providers.google.cloud.transfers.gcs_to_local import *
 from airflow.hooks.postgres_hook import PostgresHook
 
-import sys
-from pprint import pprint
-pprint(sys.path)
- 
-import logging 
-import os 
 from google.cloud import storage
 from google.cloud.storage import blob
 import psycopg2
 
+#DAG
+dag = DAG('DataUploadToPostgreSQL', description='Read a csv and upload it to a postgresSQL',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
 
 # Functions
-
 def readFileFromBucket(bucketName, fileName):
     storageClient = storage.Client()
     bucket = storageClient.bucket(bucketName)
     blob = bucket.blob(fileName)
 
-    blob.download_to_filename("test.csv")
-    
-
-   
-def databaseConnection(databaseName,user,password,host,port):
-    connection = psycopg2.connect(database=databaseName,
-                                  user = user, password = password,
-                                  host = host , port = port  )
-
-    print ("Connection succefull")            
-    #connection.close()
-    return connection
+    blob.download_to_filename("test.csv")  
 
 def uploadDataIntoDatabase():
     hook = PostgresHook()
@@ -58,31 +44,14 @@ def uploadDataIntoDatabase():
                                                                'customer_id',
                                                                'country'))
     '''
-    
     databaseConnection.commit()
     databaseConnection.close()
 
-def print_hello():
-    return 'Hello world from first Airflow DAG!'
-
-#Dags
-dag = DAG('DataUploadToPostgreSQL', description='Read a csv and upload it to a postgresSQL',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
-
-#hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
-
 # Tasks
 
-command = "pip install bs4 ;pip install psycopg2-binary ; pip install google-cloud-storage; pip install wheel; pip3 install 'apache-beam[gcp]'; pip3 install apache-airflow-providers-apache-beam;  pip3 install apache-airflow-providers;pip3 install apache-airflow-providers-apache-beam[google] --use-deprecated=legacy-resolver"
-#command = "pip install apache-airflow-providers-apache-beam[google] --use-deprecated=legacy-resolver"
-#command = "sudo su; cp -r /home/airflow/.local/lib/python3.7/site-packages/* /usr/local/lib/python3.7/dist-packages/"
-command = " sudo chown -R airflow: /usr/local/lib/python3.7/dist-packages" #pip install apache-airflow-providers-apache-beam --target /usr/local/lib/python3.7/dist-packages/"
+command = "pip install bs4 ;pip install psycopg2-binary ; pip install google-cloud-storage; pip install wheel; pip3 install 'apache-beam[gcp]'"
 installPipDependencies = BashOperator(task_id='installPipDependencies', bash_command=command,dag = dag)
 
-
-#readFile = PythonOperator(task_id='getFileFromBucket', python_callable=readFileFromBucket("de-bootcamp-ag","user_purchase.csv"), dag=dag) 
-'''
 readFile = download_file = GCSToLocalFilesystemOperator(
         task_id="getFileFromBucket",
         object_name="user_purchase.csv",
@@ -91,7 +60,5 @@ readFile = download_file = GCSToLocalFilesystemOperator(
     )
 
 uploadToDatabase = PythonOperator(task_id='uploadDataToDatabase', python_callable=uploadDataIntoDatabase, dag=dag)
-'''
-#installPipDependencies >> readFile >> uploadToDatabase
-installPipDependencies
 
+installPipDependencies >> readFile >> uploadToDatabase
